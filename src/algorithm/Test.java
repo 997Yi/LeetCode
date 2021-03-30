4c86355de471127a89a127bd6e0e5c680149aed6
WangWeiQing <775242373@qq.com>
2021-03-30 21:11:44 +0800

    快速排序

--- src/algorithm/Test.java
+++ src/algorithm/Test.java
@@ -1,13 +1,13 @@
 package algorithm;
 
-import algorithm.排序.冒泡排序;
+import algorithm.排序.快速排序;
 
 public class Test {
     public static void main(String[] args) {
         System.out.println();
-        int[] array = new int[]{4,5,2,7,2,87,46,85,12};
-        冒泡排序 sorted = new 冒泡排序();
-        sorted.sort(array);
+        int[] array = new int[]{4,85,2,7,12,87,46,5,2};
+        快速排序 sorted = new 快速排序();
+        sorted.sort(array, 0, array.length-1);
         for (int i : array) {
             System.out.print(i);
             System.out.print(" ");
--- src/algorithm/排序/快速排序.java
+++ src/algorithm/排序/快速排序.java
@@ -1,4 +1,31 @@
 package algorithm.排序;
 
 public class 快速排序 {
+    public void sort(int[] arr, int start, int end) {
+        if (start < end){
+            int left = start;
+            int right = end;
+            int pivot = arr[start];
+            while (left < right) {
+                while (left < right && arr[right] >= pivot){
+                    right--;
+                }
+                if (left < right){
+                    arr[left++] = arr[right];
+                }
+
+                while (left < right && arr[left] < pivot){
+                    left++;
+                }
+                if (left < right){
+                    arr[right--] = arr[left];
+                }
+            }
+            arr[left] = pivot;
+            sort(arr, start, left-1);
+            sort(arr, left+1, end);
+
+        }
+
+    }
 }
